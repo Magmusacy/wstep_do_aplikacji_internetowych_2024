8b39fb577023b0b39fdba3c21349fc42486286d5
Magmusacy <xpfk.098@gmail.com>
2024-11-03 17:16:31 +0100

    Add articles and simple styling that needs to be refactored in the future

--- f1_blog/assets/js/script.js
+++ f1_blog/assets/js/script.js
@@ -1,8 +1,7 @@
 const menuButton = document.querySelector(".menu-button");
 const menu = document.querySelector(".menu");
 const overlay = document.querySelector(".overlay");
-
-// let isMenuOpen = menu.dataset.menuOpen === "true";
+const articles = document.querySelectorAll(".blog article");
 
 menuButton.addEventListener("click", () => {
   let isMenuOpen = menu.dataset.menuOpen === "true";
@@ -17,8 +16,7 @@ menuButton.addEventListener("click", () => {
     // tu pomysl jak to zrobic no bo niby spoko ale
     // jednak mamy tutaj problem ze jak zmienimy cos w js to msuiy w css
     setTimeout(() => {
-      menu.classList.remove("hide-menu");
-      overlay.classList.remove("hide-overlay");
+      overlay.classList.add("hide-overlay");
     }, 500);
   } else {
     menu.dataset.menuOpen = "true";
@@ -28,3 +26,35 @@ menuButton.addEventListener("click", () => {
     overlay.classList.remove("hide-overlay");
   }
 });
+
+// This will have to be refactored.
+articles.forEach((article) => {
+  console.log(article)
+  const readMoreButton = article.querySelector(".article-footer");
+  readMoreButton.addEventListener('click', () => {
+    const articleModal = document.querySelector(`#modal-${article.id}`);
+    const closeModalButton = articleModal.querySelector("button.close-button");
+    let isModalOpen = articleModal.dataset.modalOpen === "true";
+    if (isModalOpen) {
+      articleModal.dataset.modalOpen = "false";
+      articleModal.classList.add("hide-modal");
+      articleModal.classList.remove("show-modal");
+      isModalOpen = false;
+    } else {
+      // to musi być w modalu though
+      articleModal.dataset.modalOpen = "true";
+      articleModal.classList.add("show-modal");
+      articleModal.classList.remove("hide-modal");
+      isModalOpen = true;
+    }
+    closeModalButton.addEventListener('click', () => {
+      articleModal.dataset.modalOpen = "false";
+      articleModal.classList.add("hide-modal");
+      articleModal.classList.remove("show-modal");
+      isModalOpen = false;
+    }) 
+    // articleModal.classList.remove("hide-modal");
+    console.log(`.modal-${article.id}`, articleModal);
+  })
+  console.log(readMoreButton)
+})
