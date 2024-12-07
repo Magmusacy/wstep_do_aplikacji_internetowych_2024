f2decdc07d7b4759d499df4d73d1c69feaa28ca0
Magmusacy <xpfk.098@gmail.com>
2024-12-08 00:00:52 +0100

    Add further functionality for library API

--- lab_04/app.js
+++ lab_04/app.js
@@ -4,6 +4,8 @@ const port = 3000
 const sqlite3 = require('sqlite3').verbose();
 let sql;
 
+app.use(express.json());
+
 const db = new sqlite3.Database('./database.db', sqlite3.OPEN_READWRITE, (err) => {
   if (err) {
     console.error(err.message);
@@ -11,27 +13,50 @@ const db = new sqlite3.Database('./database.db', sqlite3.OPEN_READWRITE, (err) =
   console.log('Connected to the database.');
 });
 
+// Get all books
 app.get('/api/books', (req, res) => {
   sql = `SELECT * FROM books`;
   db.all(sql, [], (err, rows) => {
     if (err) {
       throw err;
     }
-    // console.log(rows)
     res.status(200).send(rows);
+  });
+})
+
+// Get specific book
+app.get('/api/books/:id', (req, res) => {
+  sql = `SELECT * FROM books WHERE id = ?`;
+  db.all(sql, [req.params.id], (err, row) => {
+    if (err) {
+      throw err;
+    }
+    res.status(200).send(row);
+  });
+})
+
+// Add book
+app.post('/api/books', (req, res) => {
+  sql = `INSERT INTO books (title, author, year) VALUES(?, ?, ?)`;
+  db.run(sql, [req.body.title, req.body.author, req.body.year], function (err) {
+    if (err) {
+      throw err;
+    }
+    res.status(201).send({ id: this.lastID });
+  });
+})
+
+// Remove specific book
+app.delete('/api/books/:id', (req, res) => {
+  sql = `DELETE FROM books WHERE id = ?`;
+  db.run(sql, [req.params.id], function (err) {
+    if (err) {
+      throw err;
+    }
+    res.status(204).send({ id: req.params.id });
   });
 })
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
-
-sql = `SELECT * FROM books`;
-db.all(sql, [], (err, rows) => {
-  if (err) {
-    throw err;
-  }
-  rows.forEach((row) => {
-    console.log(row);
-  });
-});
--- lab_04/db_setup.js
+++ lab_04/db_setup.js
@@ -10,10 +10,10 @@ const db = new sqlite3.Database('./database.db', sqlite3.OPEN_READWRITE, (err) =
 
 
 // CREATE TABLE
-sql = `CREATE TABLE IF NOT EXISTS books(id INTEGER PRIMARY KEY, title TEXT, author TEXT, price REAL)`;
+sql = `CREATE TABLE IF NOT EXISTS books(id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT, author TEXT, year INTEGER)`;
 db.run(sql);
 
-sql = `INSERT INTO books(title, author, price) VALUES(?, ?, ?)`;
+sql = `INSERT INTO books (title, author, year) VALUES(?, ?, ?)`;
 db.run(sql, ['Harry Potter and the philosopher\'s stone', 'J.K Rowling', 1997]);
 db.run(sql, ['Lalka', 'Bolesław Prus', 1890]);
 db.run(sql, ['Zbrodnia i kara', 'Fryderyk Nietchsze', 1866]);
