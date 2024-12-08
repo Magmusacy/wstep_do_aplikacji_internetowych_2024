84e44ac55e8716e8ff90b78a432a56026b010c41
Magmusacy <xpfk.098@gmail.com>
2024-12-08 12:21:46 +0100

    Add JWT authorization for adding/deleting books

--- lab_04/app.js
+++ lab_04/app.js
@@ -1,9 +1,40 @@
 const express = require('express')
 const { Book } = require('./db_setup.js');
+const jwt = require('jsonwebtoken');
 const app = express()
 const port = 3000
+const NOT_SO_SECRET_SECRET_KEY="notsosecret";
 
 app.use(express.json());
+
+// Verify token 
+function verifyToken(req, res, next) {
+  token = req.headers['authorization'];
+
+  if (!token) {
+    return res.status(401).send('Brak tokena');
+  }
+
+  try {
+    if (!req.headers['authorization'].startsWith("Bearer ")) {
+      throw new Error('Nieprawidłowy token');
+    }
+    jwt.verify(token.substring(7, token.length), NOT_SO_SECRET_SECRET_KEY);
+    next();
+  } catch (error) {
+    return res.status(403).send('Nieprawidłowy token');
+  }
+}
+
+// Login
+app.post('/api/login', (req, res) => {
+  const { username, password } = req.body;
+  if (username === 'admin' && password === 'admin') {
+    const token = jwt.sign({ username: username }, NOT_SO_SECRET_SECRET_KEY);
+    res.status(200).send({ token });
+  }
+  res.status(401).send('Nieprawidłowe dane');
+})
 
 // Get all books
 app.get('/api/books', async (req, res) => {
@@ -18,7 +49,7 @@ app.get('/api/books/:id', async (req, res) => {
 })
 
 // Add book
-app.post('/api/books', async (req, res) => {
+app.post('/api/books', verifyToken, async (req, res) => {
   const newBook = await Book.create({
     title: req.body.title,
     author: req.body.author,
@@ -29,8 +60,12 @@ app.post('/api/books', async (req, res) => {
 })
 
 // Remove specific book
-app.delete('/api/books/:id', async (req, res) => {
+app.delete('/api/books/:id', verifyToken, async (req, res) => {
   const removeBook = await Book.findByPk(req.params.id);
+  if (!removeBook) {
+    res.status(404).send('Book not found');
+    return;
+  }
   await removeBook.destroy();
   res.status(200).send({ id: req.params.id });
 })
